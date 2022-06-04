9ef1de6b8485b410ab0876a1531f7fa28f7f3a9a
Anarik <106695465+Anariks@users.noreply.github.com>
2022-06-04 16:05:14 +0300

    My name is myName

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+string hello="Hello, Wolrld";
+string myName="Anar";
+Console.WriteLine($"{hello} my name is {myName}");
